a06ad5872f30737e1cb625e80d08cbd3a370c3f9
masha <maryged@gmail.com>
2016-03-29 20:31:43 -0400

    fxns

--- index.js
+++ index.js
@@ -76,21 +76,10 @@ var line = d3.svg.line()
         //console.log(y( parseFloat( d.value.replace(',','' ) )) );
 	return y( parseFloat( d.value.replace(',' , '') ) );
     });
-/*
-var line = d3.svg.line()
-    .x(function(d) { //d shall be the number it is on the list
-        console.log(x(d));
-        return x(d);
-    })
-    .y(function(d) {
-        console.log(y(d));
-	return y(d)
-    });
-*/
 
 var x = d3.scale.linear().domain([2015,2017]).range([80, width-80]);
 //var x = d3.scale.linear().domain([0,10]).range([80, width-80]);
-var y = d3.scale.linear().domain([0, 1000000]).range([height-80, 80]);
+var y = d3.scale.linear().domain([0, 10000]).range([height-80, 80]);
 
 var graph = d3.select('#graph').append('svg:svg')
     .attr('width', width + m[1] + m[3])
@@ -132,16 +121,20 @@ for(var i=0;i<f.length;i++){
 console.log(States.length);
 // States is an ARRAY of DICTIONARIES
 
+function graphState(i) {
+    graph.append('svg:path').attr('d', line(States[i]));
+    console.log('State: '+f[i][0]);
+}
 
 //////////HERE IS THE GRAPH ALL
-for (var i = 0; i < States.length; i++){
+/*for (var i = 0; i < States.length; i++){
   for (var i2 = 0; i2<3; i++){
     graph.append('svg:path').attr('d', line(States[i]));
   }
-}
+}*/
 //  graph.append('svg:path').attr('d', line(testDat));
 //graph.append('svg:path').attr('d', line(f[0]));
-
+graphState(0);
 function addStateNames(){
     var s = document.getElementById("stateNames");
     for(var i=0;i<f.length;i++){
@@ -153,3 +146,10 @@ function addStateNames(){
 
 addStateNames();
 
+graph.addEventListener('scroll', function(e) {
+    if (stateIndex<=50) {
+	stateIndex++;
+	graphState(stateIndex);
+    }
+});
+    
